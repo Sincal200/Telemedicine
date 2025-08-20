da11043c6b3232194cb4182c652c3947fa0be977
Sincal200 <sincaljulio200@gmail.com>
2025-08-20 17:31:51 +0000

    Add Dockerfile and Nginx configuration for React app deployment; refactor routing in App component

--- Frontend/telemedicine-frontend/src/App.jsx
+++ Frontend/telemedicine-frontend/src/App.jsx
@@ -1,28 +1,23 @@
-import { BrowserRouter as Router, Routes, Route} from 'react-router-dom'
-import { useState } from 'react'
+import { Routes, Route } from 'react-router-dom';
 import Home from './pages/Home.jsx';
 import About from './pages/About.jsx';
 import Login from './pages/Login.jsx';
 import Video from './pages/Video.jsx';
 import Register from './pages/Register';
 import Dashboard from './pages/Dashboard.jsx';
-import './App.css'
+import './App.css';
 
 function App() {
   return (
-    
-    <Router>
-      <Routes>
-        <Route path="/" element={<Login />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/home" element={<Home />} />
-        <Route path="/video" element={<Video />} />
-        <Route path="/register" element={<Register />} />
-        <Route path="/dashboard" element={<Dashboard />} />
-      </Routes>
-    </Router>
+    <Routes>
+      <Route path="/" element={<Login />} />
+      <Route path="/about" element={<About />} />
+      <Route path="/home" element={<Home />} />
+      <Route path="/video" element={<Video />} />
+      <Route path="/register" element={<Register />} />
+      <Route path="/dashboard" element={<Dashboard />} />
+    </Routes>
   );
 }
 
-
-export default App
+export default App;
